d0e48860d8898cfb0503dce478e6f3cf192faf21
snailery <87980202+snailery@users.noreply.github.com>
2025-04-09 22:34:54 +0800

    added mobile resonsiveness

--- app/components/App/Footer.jsx
+++ app/components/App/Footer.jsx
@@ -3,11 +3,11 @@ import MailIcon from "../../assets/icons/MailIcon"
 
 export default function Footer({ sitemap }) {
   return (
-    <footer className="bg-primary font-body text-sm p-12.5 pb-25 relative absolute bottom-0 flex w-full gap-7">
+    <footer className="bg-primary font-body text-sm md:p-12.5 md:pb-25 p-7.5 pb-15 relative absolute bottom-0 flex md:flex-row flex-col w-full md:gap-7 gap-12.5">
       {sitemap.map(link =>
         <div className="flex flex-col gap-3 px-2.5">
           <h1 className="text-white/50 text-sm/6 font-semibold uppercase">{link.heading}</h1>
-          <ul className="text-white flex flex-col gap-4.5">
+          <ul className="text-white flex md:flex-col flex-row md:gap-4.5 gap-8">
             {link.sublinks.map(sublink =>
               <li><a href={sublink.href}>{sublink.name}</a></li>
             )}
@@ -15,8 +15,8 @@ export default function Footer({ sitemap }) {
         </div>
       )}
       <div className="flex flex-col gap-3 px-2.5">
-        <h1 className="text-white/50 font-semibold uppercase">Contact</h1>
-        <ul className="text-white flex flex-col gap-4.5">
+        <h1 className="text-white/50 text-sm/6 font-semibold uppercase">Contact</h1>
+        <ul className="text-white flex flex-col md:gap-4.5 gap-3">
           <li className="flex items-center gap-1.5">
             <PhoneIcon className="stroke-white fill-none h-4" />
             (+63) 915 560 6578
