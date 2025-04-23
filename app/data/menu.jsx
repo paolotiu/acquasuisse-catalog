b014ae452fb1d000df2ba3dc27eb54f19fb38cae
paolotiu <paolotiu17@gmail.com>
2025-04-23 15:27:01 +0800

    Fix about mobile nav

--- app/data/menu.jsx
+++ app/data/menu.jsx
@@ -1,81 +1,80 @@
 export const menu = [
   {
     key: 1,
-    heading: "Discover",
-    href: "#",
+    heading: 'Discover',
+    href: '#',
     sublinks: [
       {
-        name: "New In",
-        href: "#"
+        name: 'New In',
+        href: '#',
       },
       {
-        name: "Best Sellers",
-        href: "#"
+        name: 'Best Sellers',
+        href: '#',
       },
       {
-        name: "View All",
-        href: "#"
-      }
-    ]
+        name: 'View All',
+        href: '#',
+      },
+    ],
   },
   {
     key: 2,
-    heading: "Collections",
-    href: "#",
+    heading: 'Collections',
+    href: '#',
     sublinks: [
       {
-        name: "Unisex",
-        href: "/catalogue"
+        name: 'Unisex',
+        href: '/catalogue',
       },
       {
-        name: "For Men",
-        href: "#"
+        name: 'For Men',
+        href: '#',
       },
       {
-        name: "For Women",
-        href: "#"
-      }
+        name: 'For Women',
+        href: '#',
+      },
     ],
   },
   {
     key: 3,
-    heading: "About",
-    href: "/about",
+    heading: 'About',
+    href: '#',
     sublinks: [
       {
-        name: "About Us",
-        href: "/about/about-us"
+        name: 'About Us',
+        href: '/about/about-us',
       },
       {
-        name: "Branches",
-        href: "/about/branches"
+        name: 'Branches',
+        href: '/about/branches',
       },
       {
-        name: "FAQs",
-        href: "/about/faq"
-      }
-    ]
-
-  }
-]
+        name: 'FAQs',
+        href: '/about/faq',
+      },
+    ],
+  },
+];
 
 const socials = {
-  heading: "Socials",
-  href: "#",
+  heading: 'Socials',
+  href: '#',
   sublinks: [
     {
-      name: "Instagram",
-      href: "#"
+      name: 'Instagram',
+      href: '#',
     },
     {
-      name: "Facebook",
-      href: "#"
+      name: 'Facebook',
+      href: '#',
     },
     {
-      name: "Shopee",
-      href: "#"
-    }
-  ]
-}
+      name: 'Shopee',
+      href: '#',
+    },
+  ],
+};
 
-export const sitemap = menu.concat(socials)
+export const sitemap = menu.concat(socials);
